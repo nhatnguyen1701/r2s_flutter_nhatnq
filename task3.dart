6d4f8c373087f985daabda163fa365a6e1d0cc9d
nhatnguyen1701 <nguyenquocnhat.tk4@gmail.com>
2025-03-16 11:08:06 +0700

    Add files via upload

---
+++ task3.dart
@@ -0,0 +1,10 @@
+void main() {
+  String name = 'Nguyen Quoc Nhat';
+  int age = 21;
+  String address = '128 Thai thi boi, Da Nang, Vietnam';
+
+  print('Thong Tin Ca Nhan:');
+  print('Name: $name');
+  print('Age: $age');
+  print('Address: $address');
+}
